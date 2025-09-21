c4eb2dc59ada0464770926663409839fefabeb9e
sadique-mohammed <code.sadique@gmail.com>
2025-09-21 15:36:14 +0530

    added loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader } from "lucide-react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Loader size={30} strokeWidth={2} className="animate-spin" />
+    </div>
+  );
+};
+
+export default Loading;
