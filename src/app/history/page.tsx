9238c08b5bc0f401e70d5bab0eaec0b0f4f9adcc
sadique-mohammed <code.sadique@gmail.com>
2025-09-24 02:58:29 +0530

    temporary

---
+++ src/app/history/page.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/components/dashboard/navbar";
+import { Features } from "@/components/landing";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <Features />
+      <Features />
+      <Features />
+      <Features />
+      <Features />
+    </>
+  );
+};
+
+export default page;
