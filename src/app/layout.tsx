67148b3989628a1fe14edac829510a5101f7497b
sadique-mohammed <code.sadique@gmail.com>
2025-09-27 03:53:53 +0530

    added Vercel Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
@@ -55,6 +56,7 @@ export default function RootLayout({
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           {children}
           <SpeedInsights />
+          <Analytics />
         </body>
         <script src="https://elfsightcdn.com/platform.js" async></script>
         <div
